b47ab089b5756763154fc5ae7d3ad95bc85ad100
louxsdon <louisayivi.dev@gmail.com>
2023-07-02 10:30:17 +0000

    add registration module

--- app/register.js
+++ app/register.js
@@ -14,9 +14,8 @@ import { useAuth } from "../lib/services/auth";
 
 axios.defaults.baseURL = API_URL;
 
-export default function Login() {
-  const { login, errors } = useAuth((state) => state);
-  const router = useRouter();
+export default function Register() {
+  const { register, errors } = useAuth((state) => state);
 
   const form = useFormik({
     initialValues: {
@@ -25,7 +24,7 @@ export default function Login() {
     },
     onSubmit: (values) => {
       // alert(JSON.stringify(values));
-      login(values);
+      register(values);
     },
   });
 
@@ -39,27 +38,33 @@ export default function Login() {
     <ScrollView style={tw`bg-violet-50`}>
       {/* forms */}
       <View style={` `}>
-        <View style={`h-[300px]  bg-[#790e4c]`}>
+        <View style={`h-[220px]  bg-[#790e4c]`}>
           <Image
             style={tw`h-[100%] w-full  bg-[#ffefba] pr-3`}
             source={require("../assets/tailor_06.png")}
             resizeMode="cover"
           />
         </View>
-        <View style={"p-12"}>
+        <View style={"p-12 pt-8"}>
           <View style={`items-center mb-4 justify-center `}>
             <Text style={`font-black text-6xl text-[#790e4c]`}>Frangalo </Text>
             <Text style={`font-black text-2xl text-[#790e4c]`}>
               Dress Making
             </Text>
-            <Text style={``}>Sign In to your account</Text>
+            <Text style={``}>Account Registration</Text>
           </View>
+          <Input
+            onChangeText={form.handleChange("name")}
+            value={form.values.name}
+            placeholder="Name"
+          />
           <Input
             keyboardType="email-address"
             onChangeText={form.handleChange("email")}
             icon="person"
             value={form.values.email}
             placeholder="Email address"
+            style={"mt-2"}
           />
           <Input
             onChangeText={form.handleChange("password")}
@@ -67,6 +72,14 @@ export default function Login() {
             value={form.values.password}
             secureTextEntry={true}
             placeholder="Password"
+            style={"mt-2"}
+          />
+          <Input
+            onChangeText={form.handleChange("password_confirmation")}
+            icon="lock"
+            value={form.values.password_confirmation}
+            secureTextEntry={true}
+            placeholder="Confirm Password"
             style={"my-2"}
           />
 
@@ -76,13 +89,13 @@ export default function Login() {
             style="bg-[#790e4c] mt-1 border-2 rounded-md border-[#b30269]"
             innerStyle="px-5 p-2 text-[#ffceeb] text-xl font-semibold"
           >
-            Login
+            Register
           </Button>
 
           {/* dont have account */}
           <View style={`flex-row justify-center mt-8`}>
             <Text style={`text-slate-400`}>
-              <Link href={"/register"}>Don't have account? Register</Link>
+              <Link href={"/"}>Already have account? Login</Link>
             </Text>
           </View>
         </View>
