b3f88d08b81478df04a32c5287caeab71ed94ee6
louxsdon <louisayivi.dev@gmail.com>
2023-07-02 11:44:41 +0000

    customer  update

--- app/home/customers/[customer].js
+++ app/home/customers/[customer].js
@@ -3,7 +3,7 @@ import { useLocalSearchParams, useRouter } from "expo-router";
 import View from "../../../components/View";
 import Input from "../../../components/Input";
 import Button from "../../../components/Button";
-import { HStack, Radio, StatusBar } from "native-base";
+import { HStack, Radio, ScrollView, StatusBar } from "native-base";
 import Text from "../../../components/Text";
 import Loader from "../../../components/Loader";
 import {
@@ -12,6 +12,8 @@ import {
   useCustomerUpdate,
 } from "../../../lib/services/customers";
 import { useFormik } from "formik";
+import tw from "../../../lib/tailwind";
+import { Alert } from "react-native";
 
 export default function Update() {
   const router = useRouter();
@@ -38,45 +40,53 @@ export default function Update() {
   }
 
   return (
-    <View style={"relative flex-1 bg-white"}>
+    <ScrollView style={tw`relative flex-1 bg-white`}>
       <View style={"bg-white p-4"}>
-        <Input
-          onChangeText={form.handleChange("name")}
-          icon="person"
-          value={form.values?.name}
-          placeholder="Customer Name"
-          style={"mb-4"}
-        />
-        <Input
-          onChangeText={form.handleChange("phone")}
-          icon="person"
-          value={form.values?.phone}
-          placeholder="Customer Phone"
-          style={"mb-4"}
-        />
-        <Radio.Group
-          name="gender"
-          accessibilityLabel="favorite number"
-          value={form.values?.gender}
-          onChange={form.handleChange("gender")}
-        >
-          <Radio value="male" my={1}>
-            Male
-          </Radio>
-          <Radio value="female" my={1}>
-            Female
-          </Radio>
-        </Radio.Group>
+        <DataView label={"NAME"} value={data.name} />
+        <DataView label={"PHONE"} value={data.phone} />
+        <DataView label={"GENDER"} value={data.gender} />
 
-        <Button
-          onPress={form.submitForm}
-          rounded
-          style="bg-[#790e4c] mt-8 border-2 rounded-md border-[#b30269]"
-          innerStyle="px-5 p-2 text-[#ffceeb] text-xl font-semibold"
-        >
-          Update Customer Details
-        </Button>
+        <View style={"px-3"}>
+          <View style={"flex flex-row justify-between"}>
+            <Button
+              onPress={() => router.push(`/home/customers/edit/${data.id}`)}
+              rounded
+              style="bg-orange-600 px-4 py-[2px] mt-8 border- rounded-md border-[#b30269] mr-4"
+              innerStyle="px-5 p-2 text-orange-200 text-xl font-semibold"
+            >
+              Edit Customer
+            </Button>
+            <Button
+              onPress={() => Alert.prompt("Delete this item?")}
+              rounded
+              style="bg-red-700 px-4 py-[2px] mt-8  rounded-md border-red-200"
+              innerStyle="px-8 p-2 text-red-200 text-xl font-semibold"
+            >
+              Delete
+            </Button>
+          </View>
+        </View>
       </View>
+    </ScrollView>
+  );
+}
+
+function DataView({ label, placeholder, icon, onChangeText, value }) {
+  return (
+    <View style={"mb-4"}>
+      {label && (
+        <Text style="text-black pb-1 text-sm bg-slate-200 px-4 py-2">
+          {label}
+        </Text>
+      )}
+      <Text
+        multiline={true}
+        style={tw.style(
+          " text-slate-500 text-base capitalize px-4 py-1.5 rounded-md"
+        )}
+      >
+        {value}
+      </Text>
     </View>
   );
 }
