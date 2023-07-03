b350321a328f166119bde0cf992e7f2311f9ec94
louxsdon <louisayivi.dev@gmail.com>
2023-07-03 07:44:05 +0000

    add loading indicator

--- app/home/customers/create.js
+++ app/home/customers/create.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import View from "../../../components/View";
 import Text from "../../../components/Text";
 import Icon from "../../../components/Icon";
@@ -12,10 +12,12 @@ import Button from "../../../components/Button";
 import { useFormik } from "formik";
 import { Radio } from "native-base";
 import { useAddCustomer } from "../../../lib/services/customers";
+import Loader from "../../../components/Loader";
 
 export default function index() {
   const { addCustomer } = useAddCustomer();
   const router = useRouter();
+  const [isAdding, setIsAdding] = useState(false);
 
   const form = useFormik({
     initialValues: {
@@ -24,13 +26,15 @@ export default function index() {
       gender: "",
     },
     onSubmit: (values) => {
-      // alert(JSON.stringify(values));
+      setIsAdding(true);
       addCustomer(values, {
         onSuccess: (res) => {
+          setIsAdding(false);
           alert("Customer added successfully!");
           router.push("/home/customers");
         },
         onError: (err) => {
+          setIsAdding(false);
           const errors = err.response?.data?.errors;
           alert(JSON.stringify(errors));
           form.setErrors(errors);
@@ -39,46 +43,49 @@ export default function index() {
     },
   });
   return (
-    <View style={"relative flex-1 bg-white"}>
-      <StatusBar style="light" backgroundColor="#790e4c" />
-      <View style={"bg-white p-4"}>
-        <Input
-          onChangeText={form.handleChange("name")}
-          icon="person"
-          value={form.values.name}
-          placeholder="Customer Name"
-          style={"mb-4"}
-        />
-        <Input
-          onChangeText={form.handleChange("phone")}
-          icon="person"
-          value={form.values.phone}
-          placeholder="Customer Phone"
-          style={"mb-4"}
-        />
-        <Radio.Group
-          name="gender"
-          accessibilityLabel="favorite number"
-          value={form.values.gender}
-          onChange={form.handleChange("gender")}
-        >
-          <Radio value="male" my={1}>
-            Male
-          </Radio>
-          <Radio value="female" my={1}>
-            Female
-          </Radio>
-        </Radio.Group>
+    <>
+      <Loader isOpen={isAdding} text={"Adding Customer"} />
+      <View style={"relative flex-1 bg-white"}>
+        <StatusBar style="light" backgroundColor="#790e4c" />
+        <View style={"bg-white p-4"}>
+          <Input
+            onChangeText={form.handleChange("name")}
+            icon="person"
+            value={form.values.name}
+            placeholder="Customer Name"
+            style={"mb-4"}
+          />
+          <Input
+            onChangeText={form.handleChange("phone")}
+            icon="person"
+            value={form.values.phone}
+            placeholder="Customer Phone"
+            style={"mb-4"}
+          />
+          <Radio.Group
+            name="gender"
+            accessibilityLabel="favorite number"
+            value={form.values.gender}
+            onChange={form.handleChange("gender")}
+          >
+            <Radio value="male" my={1}>
+              Male
+            </Radio>
+            <Radio value="female" my={1}>
+              Female
+            </Radio>
+          </Radio.Group>
 
-        <Button
-          onPress={form.submitForm}
-          rounded
-          style="bg-[#790e4c] mt-8 border-2 rounded-md border-[#b30269]"
-          innerStyle="px-5 p-2 text-[#ffceeb] text-xl font-semibold"
-        >
-          Add Customer
-        </Button>
+          <Button
+            onPress={form.submitForm}
+            rounded
+            style="bg-[#790e4c] mt-8 border-2 rounded-md border-[#b30269]"
+            innerStyle="px-5 p-2 text-[#ffceeb] text-xl font-semibold"
+          >
+            Add Customer
+          </Button>
+        </View>
       </View>
-    </View>
+    </>
   );
 }
